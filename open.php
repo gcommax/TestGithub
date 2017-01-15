9471827b4bc287161673f7702b052b073617d0b5
gcommax <gcommax@gmail.com>
2017-01-15 15:01:11 +0700

    Yse

--- open.php
+++ open.php
@@ -1,36 +1,3 @@
 <div> 
-<<<<<<< HEAD
     321
 </div>
-=======
-    ABC
-    321456
-</div>
-
-
-<div> 
-    ABC
-    321456
-</div>
- 
-<div> 
-    ABC
-    321456
-</div>
-
-Toonnnnnsdlkfasdf;
-sdfg
-sdfh
-sfgh
-
-
-
-ABC123455
-
-           
-POI9876521
-
-
-
-jhausdfuafaju
->>>>>>> origin/master
