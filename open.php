c1e2c66273bac4bf1eaf7adb871e0c189a010ffd
gcommax <gcommax@gmail.com>
2017-01-15 15:00:11 +0700

    Revert Code

--- open.php
+++ open.php
@@ -1,30 +1,3 @@
 <div> 
-    ABC
-    321456
+    321
 </div>
-
-
-<div> 
-    ABC
-    321456
-</div>
- 
-<div> 
-    ABC
-    321456
-</div>
-
-Toonnnnnsdlkfasdf;
-sdfg
-sdfh
-sfgh
-
-
-<<<<<<< HEAD
-ABC123455
-=======
-POI9876521
->>>>>>> origin/master
-
-
-jhausdfuafaju
